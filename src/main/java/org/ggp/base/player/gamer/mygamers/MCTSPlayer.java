fb0d36b93b5830e62396e939bd85dd7d0517a4bc
Oluwasanya Awe <oluwasanya.awe@gmail.com>
2016-04-29 11:08:20 -0700

    new branch

---
+++ src/main/java/org/ggp/base/player/gamer/mygamers/MCTSPlayer.java
@@ -0,0 +1,77 @@
+package org.ggp.base.player.gamer.mygamers;
+
+import java.util.List;
+
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class MCTSPlayer extends SampleGamer {
+
+
+    @Override
+    public void stateMachineMetaGame(long timeout)
+    		throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+
+    }
+
+	@Override
+    public Move stateMachineSelectMove(long timeout)
+            throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+    	return bestMove(getRole(), getCurrentState());
+    }
+
+
+
+    private Move bestMove(Role role, MachineState state)
+    		throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+    	long start = System.currentTimeMillis();
+    	StateMachine game = getStateMachine();
+        List<Move> actions = game.findLegals(role, state);
+        Move action = actions.get(0);
+        int alpha = 0;
+        int beta = 100;
+        int score = 0;
+        for (int i = 0; i < actions.size(); i++) {
+        	int result = minscore(role, actions.get(i), state, alpha, beta);
+        	if (result == 100) return actions.get(i);
+        	if (result > score) {
+        		score = result;
+        		action = actions.get(i);
+        	}
+        }
+        long stop = System.currentTimeMillis();
+        notifyObservers(new GamerSelectedMoveEvent(actions, action, stop - start));
+        return action;
+    }
+
+    private int maxscore(Role role, MachineState state, int alpha, int beta)
+    		throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+    	StateMachine game = getStateMachine();
+    	if (game.findTerminalp(state)) return game.findReward(role, state);
+    	List<Move> actions = game.findLegals(role, state);
+    	for (int i = 0; i < actions.size(); i++) {
+    		int result = minscore(role, actions.get(i), state, alpha, beta);
+    		alpha = Math.max(alpha, result);
+    		if (alpha >= beta) return beta;
+    	}
+    	return alpha;
+    }
+
+    private int minscore(Role role, Move action, MachineState state, int alpha, int beta) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+    	StateMachine game = getStateMachine();
+    	for (List<Move> jointMove : game.getLegalJointMoves(state, role, action)) {
+    		MachineState newstate = game.getNextState(state, jointMove);
+    		int result = maxscore(role, newstate, alpha, beta);
+    		beta = Math.min(beta, result);
+    		if (beta <= alpha) return alpha;
+    	}
+    	return beta;
+    }
+}
