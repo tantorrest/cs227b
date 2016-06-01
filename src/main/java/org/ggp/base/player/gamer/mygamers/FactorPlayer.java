29d8e924d1cc18677545270f35af1c9ef9deabd3
mdanie2 <mdanie2@stanford.edu>
2016-06-01 16:38:35 -0700

    Failsafe

--- src/main/java/org/ggp/base/player/gamer/mygamers/FactorPlayer.java
+++ src/main/java/org/ggp/base/player/gamer/mygamers/FactorPlayer.java
@@ -1,5 +1,6 @@
 package org.ggp.base.player.gamer.mygamers;
 
+import org.ggp.base.util.statemachine.FailsafeStateMachine;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.implementation.propnet.FactorPropNetStateMachine;
@@ -13,11 +14,12 @@ public class FactorPlayer extends StablePlayer {
 		boolean factorable = sm.markFactors();
 		p("FACTORABLE? " + factorable);
 		if (!factorable) {
-			return new CachedStateMachine(new PropNetStateMachine());
+			return new CachedStateMachine(new FailsafeStateMachine(new PropNetStateMachine()));
 		} else {
-			return sm;
+			return new FailsafeStateMachine(sm);
 		}
 	}
+	@Override
 	public void p(String x){ System.out.println(x);}
 }
 
